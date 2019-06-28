b3f1d4be4dff0fef0fde8aa22fc4c851d9951264
sumatrae <sumatrae@163.com>
2019-06-28 17:27:40 +0800

    daily update

--- radar/uart.py
+++ radar/uart.py
@@ -4,6 +4,7 @@ from threading import Thread
 from queue import Queue
 import time
 import struct
+import numpy as np
 
 PARITY_DICT = {
     "None": serial.PARITY_NONE,
@@ -31,6 +32,22 @@ BAUDRATES = (9600, 19200, 38400, 57600, 115200, 230400, 460800, 500000,
              3000000, 3500000, 4000000)
 
 HEADER_SIZE = 40
+MAX_NUM_OBJECTS = 200
+OBJ_STRUCT_SIZE_BYTES = 8
+MAX_NUM_CLUSTERS = 24
+CLUSTER_STRUCT_SIZE_BYTES = 8
+MAX_NUM_TRACKERS = 24
+TRACKER_STRUCT_SIZE_BYTES = 12
+STATS_SIZE_BYTES = 16
+
+MMWDEMO_UART_MSG_DETECTED_POINTS = 1
+MMWDEMO_UART_MSG_CLUSTERS = 2
+MMWDEMO_UART_MSG_TRACKED_OBJ = 3
+MMWDEMO_UART_MSG_PARKING_ASSIST = 4
+MMWDEMO_UART_MSG_STATS = 6
+
+TLV_HEADER_LEN = 8
+OBJ_DESC_LEN = 4
 
 
 class UartConfig():
@@ -73,54 +90,116 @@ class UartReceiveThread():
         self.count = 0
 
     def run(self):
-        self.read_buff = bytes()
-        self.magic_word = 4*[0]
         while self.com.is_open:
+            try:
+                lines = self.com.read(4096)
+                if lines:
+                    self.read_buffer += lines
+                    self.find_radar_frame()
+            except Exception as e:
+                self.read_buffer = bytes()
 
-            lines = self.com.read(1024)
-            if lines:
-                self.read_buff += lines
-                hex_str = self.read_buff.hex()
-                print(hex_str)
-                # self.valid_datas = self.check_valid_package()
-                # for valid_data in self.valid_datas:
-                # self.msg_queue.put(lines)
-                if len(hex_str) >= 2 * HEADER_SIZE:
-                    index = hex_str.find("0201040306050807")
+    def find_radar_frame(self):
+        buffer_len = len(self.read_buffer)
 
-                    header = self.read_buff[index:HEADER_SIZE + index]
-                    print(len(header))
-                    self.magic_word[0], self.magic_word[1], self.magic_word[2], self.magic_word[3], \
-                    self.version, self.total_packet_lenght, self.platform, self.frame_number, \
-                    self.time_cpucycle, self.num_detected_objection, self.num_tlvs, self.sumframe_numbers \
-                        = [hex(i) for i in struct.unpack("<HHHHIIIIIIII", header)]
+        print("buffer length:", buffer_len)
 
-                    print(self.magic_word, self.version, self.total_packet_lenght, self.platform, self.frame_number, self.time_cpucycle, self.num_detected_objection, \
-                    self.num_tlvs, self.sumframe_numbers)
+        if buffer_len >= HEADER_SIZE:
+            hex_str = self.read_buffer.hex()
+            index = hex_str.find("0201040306050807")
+            if index == -1:
+                return
+            elif index > 0:
+                self.read_buffer = self.read_buffer[index//2:]
+                buffer_len = len(self.read_buffer)
+                if HEADER_SIZE > buffer_len:
+                    return
 
+            # print("Magic word found index:", index)
+            # print("Header:", hex_str[:HEADER_SIZE * 2])
+            header = self.read_buffer[:HEADER_SIZE]
+            magic_word, version, total_package_length, platform, frame_number, time_cpucycle, num_detected_objection, \
+            num_tlvs, sumframe_numbers = [i for i in struct.unpack("<QIIIIIIII", header)]
 
-                    buffer_len = len(self.read_buff)
-                    total_package_len = int(self.total_packet_lenght,16)
-                    if buffer_len >= total_package_len:
-                        self.read_buff = self.read_buff[total_package_len:]
+            if 0x0a1642 != platform:
+                self.read_buffer = self.read_buffer[HEADER_SIZE:]
+                return
 
+            # print(int(total_packet_lenght, 16))
+            if buffer_len >= total_package_length:
+                # print("package:", hex_str[:total_package_len * 2])
+                print(hex(magic_word), version, total_package_length, hex(platform), frame_number, time_cpucycle,
+                    num_detected_objection, num_tlvs, sumframe_numbers)
 
+                main_payload = self.read_buffer[HEADER_SIZE:total_package_length]
+                self.parser_main_payload(num_tlvs, main_payload)
 
-    def get_header(self, payload):
-        self.magic_word, self.version, self.total_packet_lenght, self.platform, self.frame_number, self.time_cpucycle, self.num_detected_objection, \
-        self.num_tlvs, self.sumframe_numbers = struct.unpack(">HIIIIIIII", payload[:HEADER_SIZE])
+                if total_package_length%32 != 0:
+                    total_package_length = 32*((total_package_length//32)+1)
 
-    def check_valid_package(self):
-        self.count += 1
-        return [self.count]
+                self.read_buffer = self.read_buffer[total_package_length:]
+                # print(read_buff[:])
+
+    def parser_main_payload(self, num_tlvs, main_payload):
+        for i in range(num_tlvs):
+            tlv_header = main_payload[:TLV_HEADER_LEN]
+            if len(tlv_header) != TLV_HEADER_LEN:
+                print("tlv header len err:", len(tlv_header))
+                continue
+
+            tlv_type, tlv_length = struct.unpack("<II", tlv_header)
+            tlv_payload = main_payload[TLV_HEADER_LEN:TLV_HEADER_LEN + tlv_length]
+
+            if tlv_type == MMWDEMO_UART_MSG_CLUSTERS:
+                print("MMWDEMO_UART_MSG_CLUSTERS")
+                x, y, x_size, y_size = self.get_clusters_loction(tlv_payload)
+                msg_queue.put((x, y, x_size, y_size))
+            else:
+                print("other msg")
+
+            main_payload = main_payload[TLV_HEADER_LEN + tlv_length:]
+
+    def get_clusters_loction(self, tlv_payload):
+        obj_description = tlv_payload[:OBJ_DESC_LEN]
+        obj_payload = tlv_payload[OBJ_DESC_LEN:]
+        obj_num, xyz_qformat = struct.unpack("<HH", obj_description)
+        # print(obj_num, xyz_qformat)
+        xyx_qformat = pow(1 / 2, xyz_qformat)
+
+        x = np.zeros(obj_num)
+        y = np.zeros(obj_num)
+        x_size = np.zeros(obj_num)
+        y_size = np.zeros(obj_num)
+
+        for j in range(obj_num):
+            obj = obj_payload[j * CLUSTER_STRUCT_SIZE_BYTES:(j + 1) * CLUSTER_STRUCT_SIZE_BYTES]
+            x[j], y[j], x_size[j], y_size[j] = [s for s in struct.unpack("<HHHH", obj)]
+
+        # print(x, y, x_size, y_size)
+
+        x[x > 32767] = x[x > 32767] - 65535
+        y[y > 32767] = y[y > 32767] - 65535
+        x *= xyx_qformat
+        y *= xyx_qformat
+        x_size *= xyx_qformat
+        y_size *= xyx_qformat
+        # print(x, y, x_size, y_size)
+
+        area = 4 * x_size * y_size
+        x_size[x_size > 20] = np.inf
+
+        # print(x_size, y_size)
+
+        return x, y, x_size, y_size
 
 
 def print_msg(msg_queue):
     while True:
-        time.sleep(5)
+        # time.sleep(1)
         if msg_queue.qsize():
             byte_stream = msg_queue.get()
-            print(byte_stream.hex())
+            print("get new frame:")
+            print(byte_stream)
 
 
 if __name__ == '__main__':
